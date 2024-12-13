bfb3063757cd3aa384ff3d437d0b0669899630e4
Lucas Markovsky <131155606+lumarko@users.noreply.github.com>
2024-12-13 10:21:25 -0300

    Update get-utms.js

--- get-utms.js
+++ get-utms.js
@@ -1,66 +1,34 @@
-(function () {
-    const utmParamQueryString = new URLSearchParams(window.location.search);
+document.addEventListener('DOMContentLoaded', function () {
+    const elements = document.querySelectorAll('.elementor-button'); // Seleciona links e botões
 
-    function updateUTMsInUrl(url, utmParams) {
-        const params = new URLSearchParams(url.search);
-        utmParams.forEach((value, key) => {
-            params.set(key, value);
-        });
-        url.search = params.toString();
+    function isAnchorLink(href) {
+        return href.startsWith('#') || href === '';
     }
 
-    function transferUTMs(newUrl) {
-        updateUTMsInUrl(newUrl, utmParamQueryString);
-        sessionStorage.removeItem('utmParams');
-        sessionStorage.removeItem('nextPageUrl');
-        window.location.href = newUrl.toString();
-    }
+    elements.forEach(function (element) {
+        let href = element.getAttribute('href');
 
-    function addClickListenerToLinks(links) {
-        links.forEach(function (item) {
-            item.addEventListener('click', function (event) {
-                const href = item.getAttribute('href');
-                if (href.indexOf('#') === -1) {
-                    event.preventDefault();
-                    if (item.target === '_blank') {
-                        sessionStorage.setItem('utmParams', utmParamQueryString.toString());
-                    } else {
-                        sessionStorage.setItem('nextPageUrl', href);
-                        sessionStorage.setItem('utmParams', utmParamQueryString.toString());
-                    }
-                    transferUTMs(new URL(href));
+        // Verifica se o link é uma âncora ou não tem href
+        if (!href || isAnchorLink(href)) {
+            return;
+        }
+
+        // Obtém os parâmetros UTM da URL atual
+        const urlParams = new URLSearchParams(window.location.search);
+
+        if (urlParams.toString()) {
+            // Verifica se a URL já contém os parâmetros UTM
+            const currentUrlParams = new URLSearchParams(href.split('?')[1]);
+
+            urlParams.forEach((value, key) => {
+                if (!currentUrlParams.has(key)) {
+                    currentUrlParams.set(key, value);
                 }
             });
-        });
-    }
 
-    function updateUTMsInLinks(links) {
-        links.forEach(function (item) {
-            if (item.href.indexOf('#') !== -1) {
-                const parts = item.href.split('#');
-                const newUrl = new URL(parts[0] + '#' + parts[1]);
-                updateUTMsInUrl(newUrl, utmParamQueryString);
-                item.href = newUrl.toString();
-            } else {
-                const newUrl = new URL(item.href);
-                updateUTMsInUrl(newUrl, utmParamQueryString);
-                item.href = newUrl.toString();
-            }
-        });
-    }
-
-    if (utmParamQueryString.toString()) {
-        const navLinks = document.querySelectorAll('a');
-        updateUTMsInLinks(navLinks);
-        sessionStorage.setItem('utmParams', utmParamQueryString.toString());
-    } else {
-        const utmParams = sessionStorage.getItem('utmParams');
-        if (utmParams) {
-            const navLinks = document.querySelectorAll('a');
-            updateUTMsInLinks(navLinks);
+            // Atualiza o href do botão com os parâmetros UTM
+            const newHref = href.split('?')[0] + '?' + currentUrlParams.toString();
+            element.setAttribute('href', newHref);
         }
-    }
-
-    const allLinks = document.querySelectorAll('a');
-    addClickListenerToLinks(allLinks);
-})();
+    });
+});
