534042c197f810fd42146bd4ec92abae24a72c25
Lucas Markovsky <131155606+lumarko@users.noreply.github.com>
2024-12-13 08:50:05 -0300

    Update anti-clone.js

--- anti-clone.js
+++ anti-clone.js
@@ -1,7 +1,3 @@
-//Bloquear teclas do navegador
-
-try {document.addEventListener('contextmenu', function (e) {e.preventDefault();});document.addEventListener('keydown', function (e) {if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I')) {e.preventDefault();}});document.addEventListener('selectstart', function (e) {e.preventDefault();});} catch (e) {console.warn(e); }
-
 //Anti-clone Código
 document.addEventListener("DOMContentLoaded", function() {
     if (window.location.href.indexOf(urlDomain) === 0) {
