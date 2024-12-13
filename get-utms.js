533d5dbf27506105ced9784904f5f3bcebbd673e
Lucas Markovsky <131155606+lumarko@users.noreply.github.com>
2024-12-13 09:52:54 -0300

    Update get-utms.js

--- get-utms.js
+++ get-utms.js
@@ -21,28 +21,3 @@ document.addEventListener('DOMContentLoaded', function () {
                 }
             });
         });
-
-        // Aplica a mesma lógica quando um popup do Elementor é exibido
-        jQuery(document).on('elementor/popup/show', function () {
-            const elements = document.querySelectorAll('.elementor-button');
-
-            elements.forEach(function (element) {
-                let href = element.getAttribute('href');
-
-                // Verifica se é um link com href válido
-                if (!href || href.startsWith('#')) {
-                    return;
-                }
-
-                // Obtém os parâmetros UTM da URL atual
-                const urlParams = new URLSearchParams(window.location.search);
-
-                if (urlParams.toString()) {
-                    const separator = href.includes('?') ? '&' : '?';
-                    const newHref = href + separator + urlParams.toString();
-
-                    // Atualiza o href do botão com os parâmetros UTM
-                    element.setAttribute('href', newHref);
-                }
-            });
-        });
