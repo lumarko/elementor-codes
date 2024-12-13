ac9badeb25eb75e396f14e3e611ebf16a198c1d9
Lucas Markovsky <131155606+lumarko@users.noreply.github.com>
2024-12-13 10:07:32 -0300

    Update get-utms.js

--- get-utms.js
+++ get-utms.js
@@ -1,23 +1,66 @@
-document.addEventListener('DOMContentLoaded', function () {
-            const elements = document.querySelectorAll('.elementor-button'); // Seleciona links e botões
+(function () {
+    const utmParamQueryString = new URLSearchParams(window.location.search);
 
-            elements.forEach(function (element) {
-                let href = element.getAttribute('href');
+    function updateUTMsInUrl(url, utmParams) {
+        const params = new URLSearchParams(url.search);
+        utmParams.forEach((value, key) => {
+            params.set(key, value);
+        });
+        url.search = params.toString();
+    }
 
-                // Verifica se é um link com href válido
-                if (!href || href.startsWith('#')) {
-                    return;
-                }
+    function transferUTMs(newUrl) {
+        updateUTMsInUrl(newUrl, utmParamQueryString);
+        sessionStorage.removeItem('utmParams');
+        sessionStorage.removeItem('nextPageUrl');
+        window.location.href = newUrl.toString();
+    }
 
-                // Obtém os parâmetros UTM da URL atual
-                const urlParams = new URLSearchParams(window.location.search);
-
-                if (urlParams.toString()) {
-                    const separator = href.includes('?') ? '&' : '?';
-                    const newHref = href + separator + urlParams.toString();
-
-                    // Atualiza o href do botão com os parâmetros UTM
-                    element.setAttribute('href', newHref);
+    function addClickListenerToLinks(links) {
+        links.forEach(function (item) {
+            item.addEventListener('click', function (event) {
+                const href = item.getAttribute('href');
+                if (href.indexOf('#') === -1) {
+                    event.preventDefault();
+                    if (item.target === '_blank') {
+                        sessionStorage.setItem('utmParams', utmParamQueryString.toString());
+                    } else {
+                        sessionStorage.setItem('nextPageUrl', href);
+                        sessionStorage.setItem('utmParams', utmParamQueryString.toString());
+                    }
+                    transferUTMs(new URL(href));
                 }
             });
         });
+    }
+
+    function updateUTMsInLinks(links) {
+        links.forEach(function (item) {
+            if (item.href.indexOf('#') !== -1) {
+                const parts = item.href.split('#');
+                const newUrl = new URL(parts[0] + '#' + parts[1]);
+                updateUTMsInUrl(newUrl, utmParamQueryString);
+                item.href = newUrl.toString();
+            } else {
+                const newUrl = new URL(item.href);
+                updateUTMsInUrl(newUrl, utmParamQueryString);
+                item.href = newUrl.toString();
+            }
+        });
+    }
+
+    if (utmParamQueryString.toString()) {
+        const navLinks = document.querySelectorAll('a');
+        updateUTMsInLinks(navLinks);
+        sessionStorage.setItem('utmParams', utmParamQueryString.toString());
+    } else {
+        const utmParams = sessionStorage.getItem('utmParams');
+        if (utmParams) {
+            const navLinks = document.querySelectorAll('a');
+            updateUTMsInLinks(navLinks);
+        }
+    }
+
+    const allLinks = document.querySelectorAll('a');
+    addClickListenerToLinks(allLinks);
+})();
