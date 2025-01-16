5de50e39d7e9a842efb24c25c980cd9237cc09d6
Lucas Markovsky <131155606+lumarko@users.noreply.github.com>
2025-01-16 11:34:58 -0300

    remove o body e redireciona para a URL original

--- remove-body-redirect.js
+++ remove-body-redirect.js
@@ -1,19 +1,21 @@
 //Bloquear teclado
 try {document.addEventListener('contextmenu', function (e) {e.preventDefault();});document.addEventListener('keydown', function (e) {if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I')) {e.preventDefault();}});document.addEventListener('selectstart', function (e) {e.preventDefault();});} catch (e) {console.warn(e);}
 
-// Função para limpar o conteúdo do body e adicionar o aviso
-function substituirPorAviso() {
-    // Remove todo o conteúdo existente no body
+// Função para limpar o conteúdo do body
+function limparBody() {
     document.body.innerHTML = '';
+}
 
 // Código Anti-Clonagem Completo
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     if (window.location.href.indexOf(urlDomain) === 0) {
-
+        // Caso seja o domínio correto, não faz nada
     } else {
-        substituirPorAviso();
+        // Remove o conteúdo do body
+        limparBody();
 
-        setTimeout(function() {
+        // Após 1 segundo, redireciona para a URL original
+        setTimeout(function () {
             var redirectUrl = new URL(originalUrl);
             const pathnameWithoutSlash = window.location.pathname.endsWith('/')
                 ? window.location.pathname.slice(0, -1)
