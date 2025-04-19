db4e9a6ae32cc32d14930e394477d107ab8e03aa
Lucas Markovsky <131155606+lumarko@users.noreply.github.com>
2025-04-19 03:44:48 -0300

    Update anti-clone-popup-html.js

--- anti-clone-popup-html.js
+++ anti-clone-popup-html.js
@@ -12,46 +12,72 @@ try {
 // Função para limpar o conteúdo do body e adicionar o popup customizado
 function limparBody() {
     document.body.innerHTML = `
-        <div class="window">
-            <img src="https://static.vecteezy.com/system/resources/previews/014/203/828/non_2x/warning-caution-sign-on-transparent-background-free-png.png" alt="Alerta">
-            <h1>Este site foi clonado indevidamente.</h1>
-        </div>
+        <div class="window" >
+        <div  class="1242123432"><img src="https://upload.wikimedia.org/wikipedia/commons/0/0a/Pol%C3%ADcia_Federal_do_Brasil.png" class="policia" style="width: 8vw"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Coat_of_arms_of_Brazil.svg/800px-Coat_of_arms_of_Brazil.svg.png" class="gov" style="width: 10vw"></div>
+        <h2>OPERAÇÃO CAÇA-RATO</h2><h1>PÁGINA BLOQUEADA</h1>
+        <p>O domínio acessado foi bloqueado em decorrência da "Operação CAÇA-RATO", ação integrada, com o intuito de reprimir a pirataria de produtos digitais ilegalmente fornecida pelo detentor da página solicitada e para proteger direitos autorais legítimos.</p><a href="https://www.gov.br/mj/pt-br/assuntos/sua-protecao/sedigi/crimes-digitais">Saiba mais</a></div>
 
-        <style>
-            html, body {
-                background: #ad1b1b !important;
-                margin: 0;
-                padding: 0;
-                height: 100%;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-            }
+<style>
 
-            .window {
-                background-color: white;
-                color: #ad1b1b;
-                padding: 10vw;
-                border-radius: 2vw;
-                text-align: center;
-                box-shadow: 0 0 20px rgba(0,0,0,0.5);
-            }
+.window {
+    background-color: white;
+    color: #ad1b1b;
+    padding: 5vw;
+    border-radius: 0vw;
+    text-align: center;
+    display: flex
+;
+    flex-direction: column;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+    gap: 1vw;
+    width: 50vw;
+}
 
-            .window h1 {
-                font-size: 2.5vw !important;
-                font-family: Inter, sans-serif;
-                margin: 2vw 0 0 0;
-            }
+h1 {
+    font-family: inherit;
+    font-weight: 500;
+    line-height: 1.2;
+    color: inherit
+    font-size: 2.5rem
+}
 
-            .window img {
-                width: 10vw;
-                animation: pisca 1s infinite;
-            }
+h2 {
+    font-size: 1.7vw;
+    color: black;
+    margin: 0;
+}
 
-            @keyframes pisca {
-                50% { opacity: 0; }
-            }
-        </style>
+p {
+    margin-block-start: 0.5vw;margin-block-end: 0;color: black;
+    font-weight: 500;
+    line-height: 1.3em;
+    width: 90%;
+}
+
+a {
+    background-color: transparent;
+    text-decoration: underline;
+    color: #4233cc;
+    font-weight: 500;
+}
+
+.policia {
+    width: 5vw !important;
+}
+
+.1242123432 {
+    gap: 2vw;
+    display: flex;
+    flex-direction: row;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 2vw;
+}
+
+</style>
     `;
 }
 
