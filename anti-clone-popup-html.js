c25bc6dc7a41bc731b89f351ef454033614de055
Lucas Markovsky <131155606+lumarko@users.noreply.github.com>
2025-04-19 03:51:59 -0300

    Update anti-clone-popup-html.js

--- anti-clone-popup-html.js
+++ anti-clone-popup-html.js
@@ -12,89 +12,95 @@ try {
 // Função para limpar o conteúdo do body e adicionar o popup customizado
 function limparBody() {
     document.body.innerHTML = `
-        <div class="window" >
-        <div  class="1242123432"><img src="https://upload.wikimedia.org/wikipedia/commons/0/0a/Pol%C3%ADcia_Federal_do_Brasil.png" class="policia" style="width: 8vw"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Coat_of_arms_of_Brazil.svg/800px-Coat_of_arms_of_Brazil.svg.png" class="gov" style="width: 10vw"></div>
-        <h2>OPERAÇÃO CAÇA-RATO</h2><h1>PÁGINA BLOQUEADA</h1>
-        <p>O domínio acessado foi bloqueado em decorrência da "Operação CAÇA-RATO", ação integrada, com o intuito de reprimir a pirataria de produtos digitais ilegalmente fornecida pelo detentor da página solicitada e para proteger direitos autorais legítimos.</p><a href="https://www.gov.br/mj/pt-br/assuntos/sua-protecao/sedigi/crimes-digitais">Saiba mais</a></div>
+        <div class="window">
+            <div class="logos">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/0/0a/Pol%C3%ADcia_Federal_do_Brasil.png" class="policia" alt="Polícia Federal">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Coat_of_arms_of_Brazil.svg/800px-Coat_of_arms_of_Brazil.svg.png" class="gov" alt="Governo do Brasil">
+            </div>
+            <h2>OPERAÇÃO CAÇA-RATO</h2>
+            <h1>PÁGINA BLOQUEADA</h1>
+            <p>O domínio acessado foi bloqueado em decorrência da "Operação CAÇA-RATO", ação integrada, com o intuito de reprimir a pirataria de produtos digitais ilegalmente fornecida pelo detentor da página solicitada e para proteger direitos autorais legítimos.</p>
+            <a href="https://www.gov.br/mj/pt-br/assuntos/sua-protecao/sedigi/crimes-digitais" target="_blank">Saiba mais</a>
+        </div>
 
-<style>
+        <style>
+            html, body {
+                margin: 0;
+                padding: 0;
+                height: 100vh !important;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                background: #ad1b1b;
+                font-family: Arial, sans-serif;
+            }
 
-body {
-    height: 100vh !important;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    align-content: center;
-    background: #ad1b1b;
-}
+            .window {
+                background-color: white;
+                color: #ad1b1b;
+                padding: 5vw;
+                border-radius: 0;
+                text-align: center;
+                display: flex;
+                flex-direction: column;
+                align-content: center;
+                justify-content: center;
+                align-items: center;
+                gap: 1vw;
+                width: 50vw;
+            }
 
-.window {
-    background-color: white;
-    color: #ad1b1b;
-    padding: 5vw;
-    border-radius: 0vw;
-    text-align: center;
-    display: flex
-;
-    flex-direction: column;
-    align-content: center;
-    justify-content: center;
-    align-items: center;
-    gap: 1vw;
-    width: 50vw;
-}
+            .logos {
+                gap: 2vw;
+                display: flex;
+                flex-direction: row;
+                align-content: center;
+                justify-content: center;
+                align-items: center;
+                margin-bottom: 2vw;
+            }
 
-h1 {
-    font-family: inherit;
-    font-weight: 500;
-    line-height: 1.2;
-    color: inherit
-    font-size: 2.5rem
-}
+            .policia, .gov {
+                width: 8vw;
+            }
 
-h2 {
-    font-size: 1.7vw;
-    color: black;
-    margin: 0;
-}
+            h1 {
+                font-family: inherit;
+                font-weight: 500;
+                line-height: 1.2;
+                color: inherit;
+                font-size: 2.5rem;
+                margin: 0;
+            }
 
-p {
-    margin-block-start: 0.5vw;margin-block-end: 0;color: black;
-    font-weight: 500;
-    line-height: 1.3em;
-    width: 90%;
-}
+            h2 {
+                font-size: 1.7vw;
+                color: black;
+                margin: 0;
+            }
 
-a {
-    background-color: transparent;
-    text-decoration: underline;
-    color: #4233cc;
-    font-weight: 500;
-}
+            p {
+                margin-block-start: 0.5vw;
+                margin-block-end: 0;
+                color: black;
+                font-weight: 500;
+                line-height: 1.3em;
+                width: 90%;
+            }
 
-.policia {
-    width: 8vw !important;
-}
-
-.1242123432 {
-    gap: 2vw;
-    display: flex;
-    flex-direction: row;
-    align-content: center;
-    justify-content: center;
-    align-items: center;
-    margin-bottom: 2vw;
-}
-
-</style>
+            a {
+                background-color: transparent;
+                text-decoration: underline;
+                color: #4233cc;
+                font-weight: 500;
+            }
+        </style>
     `;
 }
 
-// Código Anti-Clonagem com Redirecionamento Automático
-(function redirectIfCloned() {
-    if (window.location.href.indexOf(urlDomain) === 0) {
-        // Se o domínio estiver correto, não faz nada
-    } else {
-            limparBody();
-    }
-})();
+// Verificação do domínio e execução imediata
+if (window.location.href.indexOf(urlDomain) === 0) {
+    // Se o domínio for o correto, não faz nada
+} else {
+    limparBody(); // Substitui o body imediatamente
+}
